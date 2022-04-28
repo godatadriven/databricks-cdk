540364c9cc1bd495e04eea1b90e89ad837a0b633
Daan Rademaker <d.v.rademaker@gmail.com>
2022-04-28 11:01:01 +0200

    add code for deploying instance pools to deploy lambda

---
+++ deploy-lambda/src/databricks_cdk/instance_pools.py
@@ -0,0 +1,105 @@
+import json
+import logging
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from databricks_cdk.utils import CnfResponse, get_request, post_request
+
+logger = logging.getLogger(__name__)
+
+
+class DockerBasicAuth(BaseModel):
+    username: str
+    password: str
+
+
+class DockerImage(BaseModel):
+    url: str
+    basic_auth: Optional[DockerBasicAuth]
+
+
+class InstancePoolAwsAttributes(BaseModel):
+    first_on_demand: int = 1
+    availability: str = "SPOT_WITH_FALLBACK"
+    zone_id: str
+
+
+class InstancePoolProperties(BaseModel):
+    workspace_url: str
+    instance_pool_name: str
+    min_idle_instances: Optional[int] = None
+    max_capacity: Optional[int] = None
+    aws_attributes: Optional[InstancePoolAwsAttributes]
+    node_type_id: str
+    custom_tags: Optional[dict] = None
+    idle_instance_autotermination_minutes: Optional[int] = 60
+    enable_elastic_disk: bool = False
+    disk_spec: Optional[dict] = None
+    preloaded_spark_versions: List[str]
+    preloader_docker_images: List[DockerImage] = []
+
+
+class InstancePoolResponse(CnfResponse):
+    instance_pool_id: str
+
+
+class InstancePoolEdit(BaseModel):
+    instance_pool_id: str
+    instance_pool_name: str
+    min_idle_instances: Optional[int]
+    max_capacity: Optional[int]
+    idle_instance_autotermination_minutes: Optional[int]
+
+
+def get_instance_pools_url(workspace_url: str):
+    """Getting url for instance_pools requests"""
+    # api-endpoint
+    return f"{workspace_url}/api/2.0/instance-pools"
+
+
+def get_instance_pool_by_id(instance_pool_id: str, workspace_url: str) -> Optional[dict]:
+    """Getting instance_pool by id"""
+    body = {"instance_pool_id": instance_pool_id}
+
+    return get_request(f"{get_instance_pools_url(workspace_url=workspace_url)}/get", body=body)
+
+
+def create_or_update_instance_pool(properties: InstancePoolProperties, physical_resource_id: Optional[str]):
+    """Create or update instance pool at Databricks"""
+    url = get_instance_pools_url(properties.workspace_url)
+    current: Optional[dict] = None
+
+    if physical_resource_id is not None:
+        current = get_instance_pool_by_id(physical_resource_id, properties.workspace_url)
+
+    if current is None:
+        create_response = post_request(f"{url}/create", body=json.loads(properties.json()))
+        instance_pool_id = create_response.get("instance_pool_id")
+        return InstancePoolResponse(instance_pool_id=instance_pool_id, physical_resource_id=instance_pool_id)
+    else:
+        instance_pool_id = current.get("instance_pool_id")
+        instance_pool_edit = InstancePoolEdit(
+            instance_pool_id=instance_pool_id,
+            instance_pool_name=properties.instance_pool_name,
+            min_idle_instances=properties.min_idle_instances,
+            max_capacity=properties.max_capacity,
+            idle_instance_autotermination_minutes=properties.idle_instance_autotermination_minutes,
+        )
+
+        post_request(f"{url}/edit", body=instance_pool_edit.dict())
+        return InstancePoolResponse(instance_pool_id=instance_pool_id, physical_resource_id=instance_pool_id)
+
+
+def delete_instance_pool(properties: InstancePoolProperties, physical_resource_id: str):
+    """Delete instance pool at databricks"""
+    current = get_instance_pool_by_id(physical_resource_id, properties.workspace_url)
+
+    if current is not None:
+        body = {
+            "instance_pool_id": physical_resource_id,
+        }
+        post_request(f"{get_instance_pools_url(properties.workspace_url)}/delete", body=body)
+    else:
+        logger.warning("Already removed")
+    return CnfResponse(physical_resource_id=physical_resource_id)
