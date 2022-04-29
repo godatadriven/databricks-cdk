c090857feac62720a981b6d95dcf50ee7e9812d0
petervanthof <peter.vanthof@godatadriven.com>
2022-04-29 09:16:44 +0200

    auto format

--- deploy-lambda/tests/test_imports.py
+++ deploy-lambda/tests/test_imports.py
@@ -1,46 +1,58 @@
-
-
 def test_cluster():
     from databricks_cdk import cluster
+
 
 def test_cluster_permissions():
     from databricks_cdk import cluster_permissions
 
+
 def test_credentials():
     from databricks_cdk import credentials
+
 
 def test_dbfs_file():
     from databricks_cdk import dbfs_file
 
+
 def test_group():
     from databricks_cdk import group
+
 
 def test_handler():
     from databricks_cdk import handler
 
+
 def test_instance_profile():
     from databricks_cdk import instance_profile
+
 
 def test_job():
     from databricks_cdk import job
 
+
 def test_networks():
     from databricks_cdk import networks
+
 
 def test_secret():
     from databricks_cdk import secret
 
+
 def test_secret_scope():
     from databricks_cdk import secret_scope
+
 
 def test_storage_config():
     from databricks_cdk import storage_config
 
+
 def test_user():
     from databricks_cdk import user
 
+
 def test_utils():
     from databricks_cdk import utils
+
 
 def test_workspace():
     from databricks_cdk import workspace
