620b3c4ee383a24f1b60a81cff054d00fcfbc737
petervanthof <peter.vanthof@godatadriven.com>
2022-07-22 14:46:32 +0200

    Adding job status

--- aws-lambda/src/databricks_cdk/jobs/job_status.py
+++ aws-lambda/src/databricks_cdk/jobs/job_status.py
@@ -1,2 +1,14 @@
+from pydantic import BaseModel
+
+from databricks_cdk.utils import get_request
+
+
+class StatusJobEvent(BaseModel):
+    workspace_url: str
+    run_id: int
+
+
 def handler(event, context):
-    pass
+    parsed_event = StatusJobEvent.parse_obj(event)
+    url = f"{parsed_event.workspace_url}/api/2.1/jobs/runs/get"
+    return get_request(url, params={"run_id": parsed_event.run_id})
--- aws-lambda/src/databricks_cdk/jobs/submit_job.py
+++ aws-lambda/src/databricks_cdk/jobs/submit_job.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 
-import requests
 from pydantic import BaseModel
+
+from databricks_cdk.utils import post_request
 
 
 class JobArgs(BaseModel):
@@ -19,15 +20,7 @@ class SubmitJobEvent(BaseModel):
     job_args: JobArgs
 
 
-class ResponseRunNow(BaseModel):
-    run_id: int
-    number_in_job: Optional[int]
-
-
 def handler(event, context):
     parsed_event = SubmitJobEvent.parse_obj(event)
     url = f"{parsed_event.workspace_url}/api/2.1/jobs/run-now"
-    response = requests.post(url, json=parsed_event.job_args.json())
-    response.raise_for_status()
-    result = ResponseRunNow.parse_raw(response.text)
-    return result.dict()
+    return post_request(url, body=parsed_event.job_args.dict())
