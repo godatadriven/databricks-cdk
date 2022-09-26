81abdd703183a7465fd87dab3ee89088062d2968
Daan Rademaker <d.v.rademaker@gmail.com>
2022-09-26 16:22:54 +0200

    add job permission to typescript

---
+++ typescript/src/resources/permissions/job-permissions.ts
@@ -0,0 +1,45 @@
+import {CustomResource} from "aws-cdk-lib";
+import {Construct} from "constructs";
+
+
+interface JobPermission {
+    permission_level: string
+}
+
+export interface JobPermissionUser extends JobPermission {
+    user_name: string
+}
+
+export interface JobPermissionGroup extends JobPermission {
+    group_name: string
+}
+
+export interface JobPermissionServicePrincipal extends JobPermission {
+    service_principal: string
+}
+
+export interface JobPermissionsProperties {
+    workspaceUrl: string
+    jobId: string
+    accessControlList: Array<JobPermissionUser | JobPermissionGroup | JobPermissionServicePrincipal>
+    ownerName: string
+}
+
+export interface JobPermissionsProps extends JobPermissionsProperties {
+    readonly serviceToken: string
+}
+
+export class JobPermissions extends CustomResource {
+    constructor(scope: Construct, id: string, props: JobPermissionsProps) {
+        super(scope, id, {
+            serviceToken: props.serviceToken,
+            properties: {
+                action: "job-permissions",
+                workspace_url: props.workspaceUrl,
+                cluster_id: props.jobId,
+                access_control_list: props.accessControlList,
+                owner_name: props.ownerName
+            }
+        });
+    }
+}
