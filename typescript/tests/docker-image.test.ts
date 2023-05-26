d9ef651441ea30dba934f40cc946deac31fe0faa
Daniel Tom <danieltom@royalfloraholland.com>
2023-05-26 13:27:42 +0200

    Fix version DockerImage

--- typescript/tests/docker-image.test.ts
+++ typescript/tests/docker-image.test.ts
@@ -4,7 +4,7 @@ describe("DockerImage", () => {
     test("DockerImage version check", () => {
         const imageVersion = DockerImage.version();
 
-        const expectedVersion = `v${require('../package.json').version}`;
+        const expectedVersion = `${require('../package.json').version}`;
 
         expect(imageVersion).toBe(expectedVersion);
     });
