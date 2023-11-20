8614243822111db6b726e39b311d8d8eff6fbbb3
Daan Rademaker <d.v.rademaker@gmail.com>
2023-11-20 15:24:08 +0100

    add volume support

---
+++ aws-lambda/tests/resources/unity_catalog/test_volumes.py
@@ -0,0 +1,145 @@
+from unittest.mock import patch
+
+import pytest
+
+from databricks_cdk.resources.unity_catalog.volumes import (
+    Volume,
+    VolumeCreatedError,
+    VolumeInfo,
+    VolumeProperties,
+    VolumeResponse,
+    VolumeType,
+    create_or_update_volume,
+    create_volume,
+    delete_volume,
+    update_volume,
+)
+from databricks_cdk.utils import CnfResponse
+
+
+@patch("databricks_cdk.resources.unity_catalog.volumes.get_workspace_client")
+@patch("databricks_cdk.resources.unity_catalog.volumes.update_volume")
+@patch("databricks_cdk.resources.unity_catalog.volumes.create_volume")
+def test_create_or_update_volume_create(
+    patched_create_volume, patched_update_volume, patched_get_workspace_client, workspace_client
+):
+    patched_get_workspace_client.return_value = workspace_client
+
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+
+    create_or_update_volume(properties=mock_properties, physical_resource_id=None)
+
+    patched_create_volume.assert_called_once_with(mock_properties, workspace_client)
+    patched_update_volume.assert_not_called()
+
+
+@patch("databricks_cdk.resources.unity_catalog.volumes.get_workspace_client")
+@patch("databricks_cdk.resources.unity_catalog.volumes.update_volume")
+@patch("databricks_cdk.resources.unity_catalog.volumes.create_volume")
+def test_create_or_update_volume_update(
+    patched_create_volume, patched_update_volume, patched_get_workspace_client, workspace_client
+):
+    patched_get_workspace_client.return_value = workspace_client
+    mock_physical_resource_id = "some_id"
+    existing_volume = [VolumeInfo(volume_id=mock_physical_resource_id, name="mock_name", comment="some comment")]
+
+    workspace_client.volumes.list.return_value = existing_volume
+
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+
+    create_or_update_volume(properties=mock_properties, physical_resource_id=mock_physical_resource_id)
+
+    patched_update_volume.assert_called_once_with(
+        mock_properties, workspace_client, existing_volume[0], mock_physical_resource_id
+    )
+    patched_create_volume.assert_not_called()
+
+
+@patch("databricks_cdk.resources.unity_catalog.volumes.get_workspace_client")
+def test_create_or_update_volume_error(patch_get_workspace_client, workspace_client):
+    patch_get_workspace_client.return_value = workspace_client
+
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+
+    workspace_client.volumes.list.return_value = []
+
+    with pytest.raises(VolumeCreatedError):
+        create_or_update_volume(properties=mock_properties, physical_resource_id="some_id")
+
+
+def test_create_volume(workspace_client):
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+
+    workspace_client.volumes.create.return_value = VolumeInfo(volume_id="some_id")
+
+    response = create_volume(mock_properties, workspace_client)
+
+    assert response == VolumeResponse(name="mock_name", physical_resource_id="some_id")
+    workspace_client.volumes.create.assert_called_once_with(
+        catalog_name="catalog",
+        schema_name="schema",
+        name="mock_name",
+        volume_type=VolumeType.MANAGED,
+        comment="some comment",
+        storage_location=None,
+    )
+
+
+def test_create_volume_error(workspace_client):
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+
+    workspace_client.volumes.create.return_value = VolumeInfo(volume_id=None)
+
+    with pytest.raises(VolumeCreatedError):
+        create_volume(mock_properties, workspace_client)
+
+
+def test_update_volume(workspace_client):
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+    mock_volume_info = VolumeInfo(volume_id="some_id", full_name="catalog.schema.mock_name")
+
+    workspace_client.volumes.update.return_value = mock_volume_info
+
+    response = update_volume(mock_properties, workspace_client, mock_volume_info, "some_id")
+
+    assert response == VolumeResponse(name="mock_name", physical_resource_id="some_id")
+    workspace_client.volumes.update.assert_called_once_with(
+        full_name_arg="catalog.schema.mock_name",
+        name="mock_name",
+        comment="some comment",
+    )
+
+
+@patch("databricks_cdk.resources.unity_catalog.volumes.get_workspace_client")
+def test_delete_volume(patched_get_workspace_client, workspace_client):
+
+    patched_get_workspace_client.return_value = workspace_client
+
+    mock_properties = VolumeProperties(
+        workspace_url="https://test.cloud.databricks.com",
+        volume=Volume(catalog_name="catalog", schema_name="schema", name="mock_name", comment="some comment"),
+    )
+    response = delete_volume(mock_properties, "some_id")
+
+    assert response == CnfResponse(physical_resource_id="some_id")
+    workspace_client.volumes.delete.assert_called_once_with(
+        full_name_arg="catalog.schema.mock_name",
+    )
---
+++ typescript/src/resources/unity-catalog/unityCatalogVolume.ts
@@ -0,0 +1,40 @@
+import {CustomResource} from "aws-cdk-lib";
+import {Construct} from "constructs";
+
+enum volumeType {
+    EXTERNAL = "EXTERNAL",
+    MANAGED = "MANAGED",
+}
+
+
+export interface UnityCatalogVolumeSettings {
+    name: string
+    schema_name: string
+    catalog_name: string
+    volume_type?: volumeType
+    comment?: string
+    storage_location?: string
+
+}
+
+export interface UnityCatalogVolumeProperties {
+    workspace_url: string
+    volume: UnityCatalogVolumeSettings
+}
+
+export interface UnityCatalogVolumeProps extends UnityCatalogVolumeProperties {
+    readonly serviceToken: string
+}
+
+export class UnityCatalogVolume extends CustomResource {
+    constructor(scope: Construct, id: string, props: UnityCatalogVolumeProps) {
+        super(scope, id, {
+            serviceToken: props.serviceToken,
+            properties: {
+                action: "unity-catalog",
+                workspace_url: props.workspace_url,
+                catalog: props.volume,
+            }
+        });
+    }
+}
